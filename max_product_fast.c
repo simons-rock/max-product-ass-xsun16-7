4ddee73ea1139cb1be04b8932e0cbffe19973b7b
xsun16 <47006803+xsun16@users.noreply.github.com>
2019-01-29 19:21:23 -0500

    Update max_product_fast.c

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,20 @@
+
 #include "max_product.h"
-
+#define Swap(x, y) {int temp; temp = x; x = y; y = temp;}
 int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+        int index = 0;
+        for (int i = 1; i < size; i++){
+                if (arr[index] < arr[i]){
+                        index = i;
+                }
+        }
+        Swap(arr[index],arr[size-1]);
+        index = 0;
+        for (int j = 1; j < size-1; j++){
+                if (arr[index] < arr[j]){
+                        index = j;
+                }
+        }
+        Swap(arr[index],arr[size-2]);
+        return arr[size-1]*arr[size-2];
 }
-
