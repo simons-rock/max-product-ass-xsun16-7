72470f6dd695c01c826c7ea83d7210de809b2567
xsun16 <47006803+xsun16@users.noreply.github.com>
2019-01-29 19:22:23 -0500

    Update max_product_naive.c

--- max_product_naive.c
+++ max_product_naive.c
@@ -1,5 +1,12 @@
 #include "max_product.h"
-
+#define Max(x, y) (x > y ? x : y)
 int max_pairwise_product_naive(int * arr, int size){
-	return 0;
+        int result = 0;
+        for (int i = 0; i<size; i++){
+                for (int j = i+1; j<size; j++){
+                        result = Max(result, arr[i]*arr[j]);
+                }
+        }
+        return result;
 }
+
